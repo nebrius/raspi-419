aba2c97d50e5815a48b51c764a11c27cf4a68993
Bryan Hughes <bryan@nebri.us>
2022-06-08 09:38:25 -0500

    Set up build infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+  ],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
